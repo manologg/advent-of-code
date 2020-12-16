c40cd377950f1c752c312962ae329eb6e9c75ea9
Manuel García García <marianocucufate@gmail.com>
2020-12-16 19:30:19 +0100

    improved new_day.bash script

--- new_day.bash
+++ new_day.bash
@@ -3,12 +3,13 @@
 current_dir=$(pwd)
 amount_of_folders=$(ls | wc -l)
 
-if [[ -z amount_of_folders ]]
+if [[ -z ${amount_of_folders} ]]
 then
-    new_folder="1"
+    new_folder="01"
 else
-    last_folder=$(ls -rt1 | tail -1)
+    last_folder=$(ls -1 | tail -1 | cut -d '_' -f 1)
     let new_folder=last_folder+1
+    new_folder=$(printf "%02d" ${new_folder})
 fi
 
 mkdir ${new_folder}
